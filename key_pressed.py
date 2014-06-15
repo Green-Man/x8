eef39eba22d8f02ffed1a372c8d3011e6726caa4
Green-Man <greenman.mail@gmail.com>
2014-06-15 11:11:23 +0800

    Restore original key reader

--- key_pressed.py
+++ key_pressed.py
@@ -1,5 +1,3 @@
-"""Random code from http://code.activestate.com/recipes/134892/"""
-
 class _Getch:
     """Gets a single character from standard input.  Does not echo to the
 screen."""
@@ -17,11 +15,14 @@ class _GetchUnix:
         import tty, sys
 
     def __call__(self):
-        import sys, tty
-
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
 
